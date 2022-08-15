8f77e5228b13c32916575e875b2cc191c1a49eb7
Charlotte Strickland <charlottemarie81@gmail.com>
2022-08-15 12:52:45 -0400

    Added save

--- Desktop/react-rainbow/src/App.js
+++ Desktop/react-rainbow/src/App.js
@@ -5,7 +5,7 @@ import ColorForm from './ColorForm';
 import './App.css';
 
 function App() {
-  let [colors, setColors] = (['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
   let colorMap = colors.map((color, i) => {
     return (
       <ColorBlock key="" color={color} />
